dc229ec6161a363043267ce70d4eb535af8a5ea7
Ravi Sharma <rsravisharma38@gmail.com>
2025-09-16 15:40:34 +0530

    user profile

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -11,6 +11,7 @@ use App\Models\UserFollow;
 use App\Models\UserBlock;
 use App\Models\Product;
 use App\Models\Favorite;
+use App\Models\Purchase;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
