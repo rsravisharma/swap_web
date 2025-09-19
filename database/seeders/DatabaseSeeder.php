9c1b5d122423258775247132afb9021833bd9139
Ravi Sharma <rsravisharma38@gmail.com>
2025-09-19 17:54:30 +0530

    item bugs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
          $this->call([
             CountrySeeder::class,
             UserPreferenceSeeder::class,
+            CategoryHierarchySeeder::class,
+            PaymentMethodSeeder::class,
         ]);
     }
 }
