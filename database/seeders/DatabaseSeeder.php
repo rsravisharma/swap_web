b6da48fef98bbd09d1b1adf9aa7ab03c9ca1dd88
Ravi Sharma <rsravisharma38@gmail.com>
2025-12-24 12:45:12 +0530

    payment system started

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             CategoryHierarchySeeder::class,
             PaymentMethodSeeder::class,
             LocationSeeder::class,
+            SubscriptionPlanSeeder::class,
         ]);
     }
 }
